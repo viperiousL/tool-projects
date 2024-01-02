7923f3e9a9bc7b2375b93b5922182d80e02b6f23
libing <1275193680@qq.com>
2024-01-02 15:54:40 +0800

    代码提交

--- libing-demo/src/main/java/com/libing/libingdemo/controller/SysMenuController.java
+++ libing-demo/src/main/java/com/libing/libingdemo/controller/SysMenuController.java
@@ -43,14 +43,11 @@ public class SysMenuController {
         SysMenuVO m6 = new SysMenuVO(6L, 1L, "yiji");
         //第一种，结果为一个数组，可排序，可自定义顶级菜单父id值
         List<SysMenuVO> nodeMenus = Arrays.asList(m1, m2, m3, m4, m5, m6);
-        List<SysMenuVO> nodeMenus1 = ConvertTree.convert(nodeMenus, "id", "pid", "id", "desc", "0", "");
-        nodeMenus1.stream().forEach(SysMenuVO -> {
-            System.out.println(SysMenuVO.toString());
-        });
+        List<SysMenuVO> nodeMenus1 = ConvertTree.convert(nodeMenus, "id", "pid", "id", "desc", "0", null);
+        nodeMenus1.stream().forEach(SysMenuVO -> System.out.println(SysMenuVO.toString()));
         //第二种，结果为一个对象，顶级菜单的父id为null，只能修改方法，否则无法自定义设置
         SysMenuVO nodeMenus2 = BuildTreeUtil.buildTree(nodeMenus);
         System.out.println(nodeMenus2);
-
 
     }
 
